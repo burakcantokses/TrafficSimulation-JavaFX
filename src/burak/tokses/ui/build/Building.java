24f367562e650c0d53a30c18e59b6c1d4404ff88
Burak <75482610+burakcantokses@users.noreply.github.com>
2024-05-05 16:07:30 +0300

    building hatasız 4

--- src/burak/tokses/ui/build/Building.java
+++ src/burak/tokses/ui/build/Building.java
@@ -34,22 +34,172 @@ public class Building {
     }
 
     public Node toNode() {
-        System.out.println(x + " " + y + " " + type + " " + rotation + " " + color);
         // Assuming each grid cell is 50x50 pixels
-        int cellSize = 50;
+        double cellSize = 53.33;
 
         switch (type) {
             case 0:
                 return createType0Building(x, y, cellSize, rotation, color);
             case 1:
-                return createType1Building(x, y, cellSize, color);
+                return createType1Building(x, y, cellSize, rotation, color);
             case 2:
-                return createBuildingRectangle(x, y, cellSize, rotation, color);
+                return createType2Building(x, y, cellSize, color);
             default:
                 return null;
         }
     }
-    private Group createType0Building(int x, int y, int cellSize, int rotation, int color) {
+
+    public Group createType0Building(int x, int y, double cellSize, int rotation, int color) {
+        // Outer rectangle
+        double outerWidth = cellSize * 2.3;
+        double outerHeight = cellSize * 3.3;
+        double outerX = x * cellSize - 5;
+        double outerY = y * cellSize - 10;
+        Rectangle outerRectangle = new Rectangle(outerX, outerY, outerWidth, outerHeight);
+        outerRectangle.setArcWidth(12);
+        outerRectangle.setArcHeight(12);
+        outerRectangle.setFill(Color.web("#EFF9FE"));
+        outerRectangle.setStroke(Color.BLACK);
+
+        // Main rectangle
+        double mainX = outerX + (outerWidth - cellSize * 2) / 2; // outerRectangle'ın ortasına yerleştirildi
+        double mainY = outerY + 20; // outerRectangle'ın üst kısmına yerleştirildi ve biraz aşağı indirildi
+        Rectangle mainRectangle = new Rectangle(mainX, mainY, cellSize * 2, cellSize * 2);
+        mainRectangle.setFill(getColor(color));
+        mainRectangle.setStroke(getStrokeColor(color));
+        mainRectangle.setStrokeWidth(5);
+        mainRectangle.setArcWidth(12);
+        mainRectangle.setArcHeight(12);
+
+        // Inner rectangle
+        double innerSize = cellSize * 2 * 0.8;
+        double innerX = mainX + (cellSize * 2 - innerSize) / 2;
+        double innerY = mainY + (cellSize * 2 - innerSize) / 2;
+        Rectangle innerRectangle = new Rectangle(innerX, innerY, innerSize, innerSize);
+        innerRectangle.setFill(getColor(color));
+        innerRectangle.setArcWidth(10);
+        innerRectangle.setArcHeight(10);
+        innerRectangle.setStroke(getStrokeColor(color));
+        innerRectangle.setStrokeWidth(5);
+
+        // Create a group to hold all rectangles
+        Group group = new Group(outerRectangle, mainRectangle, innerRectangle);
+        switch (rotation) {
+            case 90:
+                group.getTransforms().add(new Rotate(270, outerX + outerWidth / 2, outerY + outerHeight / 2));
+                break;
+            case 180:
+                group.getTransforms().add(new Rotate(180, outerX + outerWidth / 2, outerY + outerHeight / 2));
+                break;
+            case 270:
+                group.getTransforms().add(new Rotate(90, outerX + outerWidth / 2, outerY + outerHeight / 2));
+                break;
+        }
+        System.out.println("Building created at " + x + ", " + y + " with rotation " + rotation + " and color " + color);
+        return group;
+    }
+
+    public Group createType1Building(int x, int y, double cellSize, int rotation, int color) {
+        // Outer rectangle
+        double outerWidth = cellSize * 2.3;
+        double outerHeight = cellSize * 3.3;
+        double outerX = x * cellSize - 5;
+        double outerY = y * cellSize - 10;
+        Rectangle outerRectangle = new Rectangle(outerX, outerY, outerWidth, outerHeight);
+        outerRectangle.setArcWidth(12);
+        outerRectangle.setArcHeight(12);
+        outerRectangle.setFill(Color.web("#EFF9FE"));
+        outerRectangle.setStroke(Color.BLACK);
+
+        // Calculate main circle position
+        double mainCircleX = (x + 1) * cellSize;
+        double mainCircleY = (y + 1.1) * cellSize;
+
+        // Main circle
+        Circle mainCircle = new Circle(mainCircleX, mainCircleY, cellSize);
+        mainCircle.setFill(getColor(color));
+        mainCircle.setStroke(getStrokeColor(color));
+        mainCircle.setStrokeWidth(5);
+
+        // Calculate inner circle radius
+        double innerRadius = cellSize * 0.85;
+
+        // Inner circle
+        Circle innerCircle = new Circle(mainCircleX, mainCircleY, innerRadius);
+        innerCircle.setFill(getColor(color));
+        innerCircle.setStroke(getStrokeColor(color));
+        innerCircle.setStrokeWidth(5);
+
+        // Create a group to hold all shapes
+        Group group = new Group(outerRectangle, mainCircle, innerCircle);
+
+        switch (rotation) {
+            case 90:
+                group.getTransforms().add(new Rotate(270, outerX + outerWidth / 2, outerY + outerHeight / 2));
+                break;
+            case 180:
+                group.getTransforms().add(new Rotate(180, outerX + outerWidth / 2, outerY + outerHeight / 2));
+                break;
+            case 270:
+                group.getTransforms().add(new Rotate(90, outerX + outerWidth / 2, outerY + outerHeight / 2));
+                break;
+        }
+        return group;
+    }
+
+
+    public Rectangle createType2Building(int x, int y, double cellSize, int color) {
+        // Create a rectangle
+        Rectangle rectangle = new Rectangle(x * cellSize, y * cellSize, cellSize, cellSize);
+
+        // Set the arc width and height to make the corners rounded
+        rectangle.setArcWidth(20);
+        rectangle.setArcHeight(20);
+
+        // Set the fill color based on the color index
+        rectangle.setFill(getColor(color));
+        rectangle.setStroke(getStrokeColor(color));
+        rectangle.setStrokeType(StrokeType.INSIDE);
+        rectangle.setStrokeWidth(2);
+
+        return rectangle;
+    }
+
+    public Color getColor(int colorIndex) {
+        switch (colorIndex) {
+            case 0:
+                return Color.web("#FECB9B");
+            case 1:
+                return Color.web("#90E4BE");
+            case 2:
+                return Color.web("#BCB5E9");
+            case 3:
+                return Color.web("#EF7E91");
+            default:
+                return Color.BLACK;
+        }
+    }
+
+    public Color getStrokeColor(int colorIndex) {
+        switch (colorIndex) {
+            case 0:
+                return Color.web("#F3B98D");
+            case 1:
+                return Color.web("#89CDB9");
+            case 2:
+                return Color.web("#AAA2D4");
+            case 3:
+                return Color.web("#DE7182");
+            default:
+                return Color.BLACK;
+        }
+    }
+
+
+
+    /*
+    //tüm şekilleri tek şekil halien gelip rotation verç.
+    private Group createType0Building(int x, int y, double cellSize, int rotation, int color) {
         // Outer rectangle
         Rectangle outerRectangle = new Rectangle();
         switch (rotation) {
@@ -101,8 +251,6 @@ public class Building {
 
         return group;
     }
-
-
     // Utility method to get color based on index
     private Color getColor(int colorIndex) {
         switch (colorIndex) {
@@ -122,7 +270,7 @@ public class Building {
         Color color = getColor(colorIndex);
         return color.darker();
     }
-    public Circle createType1Building(int x, int y, int cellSize, int color) {
+    public Circle createType1Building(int x, int y, double cellSize, int color) {
         // Create a circle for the building
         Circle circle = new Circle((x + 0.5) * cellSize, (y + 0.5) * cellSize, cellSize / 2);
 
@@ -145,7 +293,7 @@ public class Building {
         return circle;
     }
 
-    public Rectangle createBuildingRectangle(int x, int y, int cellSize, int rotation, int color) {
+    public Rectangle createBuildingRectangle(int x, int y, double cellSize, int rotation, int color) {
         // Create a rectangle for the building with rounded corners
         Rectangle rectangle = new Rectangle(x * cellSize, y * cellSize, cellSize, cellSize);
         rectangle.setArcWidth(12); // Set the arc width for rounded corners
@@ -175,4 +323,5 @@ public class Building {
 
         return rectangle;
     }
+     */
 }
