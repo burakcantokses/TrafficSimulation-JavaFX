7e34103d53c25decd74f8d77e843b8c6632f3828
Burak <75482610+burakcantokses@users.noreply.github.com>
2024-05-06 03:16:13 +0300

    roads type 2 başarılı

--- src/burak/tokses/ui/road/RoadTile.java
+++ src/burak/tokses/ui/road/RoadTile.java
@@ -23,6 +23,9 @@ public class RoadTile {
             case 0:
                 drawType0Road(group, cellWidth, cellHeight, rotation);
                 break;
+            case 2:
+                drawType2Road(group, cellWidth, cellHeight, rotation);
+                break;
         }
     }
 
@@ -38,5 +41,43 @@ public class RoadTile {
         group.getChildren().add(road);
 
     }
+
+    public void drawType2Road(Group group, double cellWidth, double cellHeight, int rotation) {
+        // Road'un dört kenarlarının koordinatlarını hesaplayın
+        double roadWidth = cellWidth; // Sağdan ve soldan 5 piksel padding
+        double roadHeight = cellHeight; // Yukarıdan ve aşağıdan 5 piksel padding
+        double roadX = x * cellWidth; // Sağdan 5 piksel padding
+        double roadY = y * cellHeight; // Yukarıdan 5 piksel padding
+
+        // Road'u çizin
+        Rectangle road = new Rectangle(roadX, roadY, roadWidth, roadHeight);
+        road.setFill(Color.web("#FEFEFE"));
+        road.setRotate(rotation);
+        group.getChildren().add(road);
+
+        // Road'ın köşelerine kareler ekle
+        double cornerSize = 5;
+        Rectangle topLeftCorner = new Rectangle(roadX, roadY, cornerSize, cornerSize);
+        topLeftCorner.setFill(Color.web("#9BC6DF"));
+        topLeftCorner.setRotate(rotation);
+
+        Rectangle topRightCorner = new Rectangle(roadX + roadWidth - cornerSize, roadY, cornerSize, cornerSize);
+        topRightCorner.setFill(Color.web("#9BC6DF"));
+        topRightCorner.setRotate(rotation);
+
+        Rectangle bottomLeftCorner = new Rectangle(roadX, roadY + roadHeight - cornerSize, cornerSize, cornerSize);
+        bottomLeftCorner.setFill(Color.web("#9BC6DF"));
+        bottomLeftCorner.setRotate(rotation);
+
+        Rectangle bottomRightCorner = new Rectangle(roadX + roadWidth - cornerSize, roadY + roadHeight - cornerSize, cornerSize, cornerSize);
+        bottomRightCorner.setFill(Color.web("#9BC6DF"));
+        bottomRightCorner.setRotate(rotation);
+
+        group.getChildren().addAll(topLeftCorner, topRightCorner, bottomLeftCorner, bottomRightCorner);
+
+
+    }
+
+
 
 }
